10759066cae29bc264b774e684385050c8e89d52
ShannonPaige <spaige@gmail.com>
2015-10-31 13:49:03 -0600

    finished implemetning phone number

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -47,4 +47,20 @@ describe 'the person view', type: :feature do
     expect(page).to_not have_content(old_number)
   end
 
+  it "has a link to delete phone numbers" do
+    person.phone_numbers.each do |phone_number|
+      expect(page).to have_link('Delete', phone_number_path(phone_number))
+    end
+  end
+
+  it "deletes phone numbers" do
+    phone = person.phone_numbers.first
+    old_number = phone.number
+
+    first(:link, 'Delete').click
+
+    expect(current_path).to eq(person_path(person))
+    expect(page).to_not have_content(old_number)
+  end
+
 end
